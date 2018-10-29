ae000bf091fc5b67629b49bd633f3689eaa8d29e
Jérémy Icard <jeremyicard@capgemini.com>
2018-10-29 14:06:31 +0100

    Création de l'application

---
+++ src/main/java/com/capgemini/referentielcuid/ReferentielCuidApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.referentielcuid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferentielCuidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferentielCuidApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/referentielcuid/ReferentielCuidApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.referentielcuid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReferentielCuidApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
